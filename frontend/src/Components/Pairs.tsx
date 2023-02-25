37eb0b1baead5dbdd1165117c7fddea2e7b6c73b
Derek Borges <dborges2143@gmail.com>
2023-02-24 21:30:54 -0500

    progress on Pairs UI

--- frontend/src/Components/Pairs.tsx
+++ frontend/src/Components/Pairs.tsx
@@ -1,9 +1,26 @@
+import React from 'react'
+import Button from '@mui/material/Button'
+import Chip from '@mui/material/Chip'
+import Divider from '@mui/material/Divider'
 import Paper from '@mui/material/Paper'
+import Stack from '@mui/material/Stack'
 import Typography from '@mui/material/Typography'
 import Grid2 from '@mui/material/Unstable_Grid2/Grid2'
-import React from 'react'
+import { usePairminatorContext } from '../context/PairminatorContext'
+import { Pairee } from '../models/interface'
+import { PairingState } from '../models/enum'
+import LinearProgress from '@mui/material/LinearProgress'
 
 export const Pairs = (): JSX.Element => {
+  const {
+    pairees,
+    availablePairees,
+    togglePaireeAvailability,
+    pairingState,
+    currentPairs,
+    generatePairs
+  } = usePairminatorContext()
+
   return (
     <>
         <Grid2 xs={12}>
@@ -11,6 +28,90 @@ export const Pairs = (): JSX.Element => {
                 <Typography component="h2" variant="h6" color="primary" gutterBottom>
                     Current Pairs
                 </Typography>
+
+                {pairingState === PairingState.INITIAL && (
+                  <>
+                    <Typography variant='body1'>
+                      No pairs have been generated yet.
+                    </Typography>
+                    <Divider sx={{ my: 1 }} />
+                    <Typography component="h2" variant="h6">
+                      Who's available to Pair? (select to toggle)
+                    </Typography>
+                    {!!pairees.length ? (
+                      <Grid2 container px={0} pt={1} spacing={1}>
+                          {pairees.map((pairee: Pairee) => (
+                            <Grid2>
+                              <Chip
+                                component={Button}
+                                size='medium'
+                                label={pairee.name}
+                                variant={availablePairees.some(p => p.id === pairee.id) ? 'filled' : 'outlined'}
+                                color={availablePairees.some(p => p.id === pairee.id) ? 'success' : 'default'}
+                                onClick={() => togglePaireeAvailability(pairee)}
+                                sx={{
+                                  width: 90,
+                                  height: 35,
+                                  fontSize: 14
+                                }}
+                                />
+                            </Grid2>
+                          ))}
+                      </Grid2>
+                    ) : (
+                      <Typography variant='body2'>
+                        Add 2 or more pairees to generate pairs
+                      </Typography>
+                    )}
+                  </>
+                )}
+                {pairingState === PairingState.GENERATING && (
+                  <LinearProgress color='inherit' sx={{ mt: 2 }} />
+                )}
+                {pairingState === PairingState.GENERATED && (
+                  <></>
+                )}
+
+                <Stack direction="row" pt={3} pb={2} spacing={2}>
+                  {pairingState === PairingState.INITIAL && (
+                    <Stack direction='row' alignItems='center' spacing={2}>
+                      <Button
+                        color="primary"
+                        variant='contained'
+                        size='large'
+                        disabled={availablePairees.length < 2}
+                        onClick={generatePairs}
+                      >
+                        Generate
+                      </Button>
+                      {availablePairees.length < 2 && (
+                        <Typography
+                          variant='caption'
+                          color='error'
+                        >
+                          2 or more pairees must be available to generate pairs.
+                        </Typography>
+                      )}
+                    </Stack>
+                  )}
+                  {pairingState === PairingState.GENERATED && (
+                    <>
+                      <Button
+                        color="success"
+                        variant='contained'
+                      >
+                        Assign
+                      </Button>
+                      <Button
+                        color="warning"
+                        variant='contained'
+                      >
+                        Reset
+                      </Button>
+                    </>
+                  )}
+                </Stack>
+                
             </Paper>
         </Grid2>
     </>
--- frontend/src/context/PairminatorContext.tsx
+++ frontend/src/context/PairminatorContext.tsx
@@ -1,11 +1,17 @@
 import { createContext, useContext, useState } from "react";
-import { Pairee } from "../models/pair";
+import { Pair, Pairee } from "../models/interface";
 import { cloneDeep } from 'lodash'
+import { PairingState } from "../models/enum";
 
 export interface PairminatorContextT {
     pairees: Pairee[];
     addPairee: (name: string) => void;
     updatePairee: (id: number, updatedName: string) => void;
+    availablePairees: Pairee[];
+    togglePaireeAvailability: (pairee: Pairee) => void;
+    pairingState: PairingState;
+    currentPairs: Pair[];
+    generatePairs: () => void;
 }
 
 export const PairminatorContext = createContext<PairminatorContextT | undefined>(undefined);
@@ -23,20 +29,24 @@ interface Props {
 }
 
 export const PairminatorProvider: React.FC<Props> = ({ children }) => {
-    const [pairees, setPairees] = useState<Pairee[]>([]);
 
+    /* BEGIN PAIREES */
+    const [pairees, setPairees] = useState<Pairee[]>([]);
+    const [availablePairees, setAvailablePairees] = useState<Pairee[]>(pairees);
+    
     const addPairee = (name: string) => {
         const newId: number = pairees.length > 0
-            ? Math.max(...(pairees.map(p => p.id))) + 1
-            : 1;
-
+        ? Math.max(...(pairees.map(p => p.id))) + 1
+        : 1;
+        
         const newPairee: Pairee = {
             id: newId,
             name,
         }
         setPairees([...pairees, newPairee]);
+        setAvailablePairees([...availablePairees, newPairee])
     }
-
+    
     const updatePairee = (id: number, updatedName: string) => {
         const pairee = pairees.find(p => p.id === id);
         if (pairee !== undefined) {
@@ -47,10 +57,37 @@ export const PairminatorProvider: React.FC<Props> = ({ children }) => {
         }
     }
 
+    const togglePaireeAvailability = (pairee: Pairee) => {
+        const availableCopy: Pairee[] = cloneDeep(availablePairees)
+        const currentlyAvailable: boolean = availableCopy.some(p => p.id === pairee.id)
+        if (currentlyAvailable) {
+        const index: number = availableCopy.findIndex(p => p.id === pairee.id)
+        availableCopy.splice(index, 1)
+        } else {
+        availableCopy.push(pairee)
+        }
+        setAvailablePairees([...availableCopy])
+    }
+    /* END PAIREES */
+
+    /* BEGIN PAIRS */
+    const [pairingState, setPairingState] = useState<PairingState>(PairingState.INITIAL)
+    const [currentPairs, setCurrentPairs] = useState<Pair[]>([]);
+
+    const generatePairs = () => {
+        setPairingState(PairingState.GENERATING)
+    }
+    /* END PAIRS */
+    
     const contextValue: PairminatorContextT = {
         pairees,
         addPairee,
-        updatePairee
+        updatePairee,
+        availablePairees,
+        togglePaireeAvailability,
+        pairingState,
+        currentPairs,
+        generatePairs,
     };
 
     return (
