f8fc8949e572780ddc011d12701e6d0fe834dbd7
Derek Borges <dborges2143@gmail.com>
2023-03-01 23:49:31 -0500

    only load pairing dashboard after loading a project

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -3,8 +3,17 @@ import Grid2 from '@mui/material/Unstable_Grid2/Grid2'
 import { History } from './History'
 import { Pairees } from './Pairees'
 import { Pairs } from './Pairs'
+import { usePairminatorContext } from '../context/PairminatorContext'
+import { useNavigate } from 'react-router'
 
 export const Dashboard = (): JSX.Element => {
+    const { projectId } = usePairminatorContext()
+    const navigate = useNavigate()
+
+    if (!projectId) {
+        navigate({ pathname: '/' })
+    }
+
     return (
         <>
             <Grid2 xs={12} md={8}>
