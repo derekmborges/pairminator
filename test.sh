09bf341c96c92705a29e88390eb42017669a200b
Derek Borges <dborges2143@gmail.com>
2023-04-20 10:46:41 -0400

    try fixing the script

--- test.sh
+++ test.sh
@@ -1,19 +1,29 @@
+#!/usr/bin/env bash
+
 echo "Waiting for Firebase emulators..."
 ./wait-for-it.sh 127.0.0.1:4000 -t 60 --strict
-if [[ $? -ne 0 ]]; then exit 1; fi
+if [[ $? -ne 0 ]]; then
+    exit 1
+fi
 echo "Emulator is up"
 
 ./wait-for-it.sh 127.0.0.1:8080 -t 60 --strict --
-if [[ $? -ne 0 ]]; then exit 1; fi
+if [[ $? -ne 0 ]]; then
+    exit 1
+fi
 echo "Firestore is up"
 
 ./wait-for-it.sh 127.0.0.1:9099 -t 60 --strict --
-if [[ $? -ne 0 ]]; then exit 1; fi
+if [[ $? -ne 0 ]]; then
+    exit 1
+fi
 echo "Auth is up"
 
 echo "Waiting for UI..."
 ./wait-for-it.sh localhost:3000 -t 60 --strict --
-if [[ $? -ne 0 ]]; then exit 1; fi
+if [[ $? -ne 0 ]]; then
+    exit 1
+fi
 echo "UI is up"
 
 echo "Starting tests"
