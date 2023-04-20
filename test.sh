be3ad3d6a6552eb7bb651bd17b4d378fad3f3304
Derek Borges <dborges2143@gmail.com>
2023-04-20 00:17:34 -0400

    cleanly quit emulators

--- test.sh
+++ test.sh
@@ -1,8 +1,10 @@
+echo "Waiting for Firebase emulators..."
+./wait-for-it.sh 127.0.0.1:4000 -t 60 --strict -- echo "Emulator is up"
+./wait-for-it.sh 127.0.0.1:8080 -t 60 --strict -- echo "Firestore is up"
+./wait-for-it.sh 127.0.0.1:9099 -t 60 --strict -- echo "Auth is up"
+
 echo "Waiting for UI..."
 ./wait-for-it.sh localhost:3000 -t 60 --strict -- echo "UI is up"
-
-echo "Waiting for Firebase emulators..."
-./wait-for-it.sh 127.0.0.1:4000 -t 60 --strict -- echo "Firebase is up"
 
 echo "Starting tests"
 yarn test;
