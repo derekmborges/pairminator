f5d91d3bba9aa729de440d1e5f82ac624608da12
Derek Borges <dborges2143@gmail.com>
2023-04-20 09:01:27 -0400

    make wait-for-it fail if services aren't up

--- test.sh
+++ test.sh
@@ -1,10 +1,20 @@
 echo "Waiting for Firebase emulators..."
-./wait-for-it.sh 127.0.0.1:4000 -t 60 --strict -- echo "Emulator is up"
-./wait-for-it.sh 127.0.0.1:8080 -t 60 --strict -- echo "Firestore is up"
-./wait-for-it.sh 127.0.0.1:9099 -t 60 --strict -- echo "Auth is up"
+./wait-for-it.sh 127.0.0.1:4000 -t 60 --strict
+if [[ $? -ne 0 ]]; then exit 1; fi
+echo "Emulator is up"
+
+./wait-for-it.sh 127.0.0.1:8080 -t 60 --strict --
+if [[ $? -ne 0 ]]; then exit 1; fi
+echo "Firestore is up"
+
+./wait-for-it.sh 127.0.0.1:9099 -t 60 --strict --
+if [[ $? -ne 0 ]]; then exit 1; fi
+echo "Auth is up"
 
 echo "Waiting for UI..."
-./wait-for-it.sh localhost:3000 -t 60 --strict -- echo "UI is up"
+./wait-for-it.sh localhost:3000 -t 60 --strict --
+if [[ $? -ne 0 ]]; then exit 1; fi
+echo "UI is up"
 
 echo "Starting tests"
 yarn test;
