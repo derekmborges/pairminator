8ab1a0ed6c19b5b027729e8fd7ece6ae9655c25c
Derek Borges <dborges2143@gmail.com>
2023-04-19 23:23:12 -0400

    make waits strict

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 echo "Waiting for UI..."
-./wait-for-it.sh http://localhost:3000 -t 60 -- echo "UI is up"
+./wait-for-it.sh localhost:3000 -t 60 --strict -- echo "UI is up"
 
 echo "Waiting for Firebase emulators..."
-./wait-for-it.sh http://127.0.0.1:4000 -t 60 -- echo "Firebase is up"
+./wait-for-it.sh 127.0.0.1:4000 -t 60 --strict -- echo "Firebase is up"
 
 echo "Starting tests"
 yarn test;
