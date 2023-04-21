ef317e73e4b6b7471b8196755f01b1d3de21b1e0
Derek Borges <dborges2143@gmail.com>
2023-04-21 12:45:14 -0400

    trying emulators:exec

--- test.sh
+++ test.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
-echo "Waiting for Firebase emulators..."
+# echo "Waiting for Firebase emulators..."
 
-./wait-for-it.sh 127.0.0.1:8080 -t 60
-if [[ $? -ne 0 ]]; then
-    exit 1
-fi
-echo "Firestore is up"
+# ./wait-for-it.sh 127.0.0.1:8080 -t 60
+# if [[ $? -ne 0 ]]; then
+#     exit 1
+# fi
+# echo "Firestore is up"
 
-./wait-for-it.sh 127.0.0.1:9099 -t 60
-if [[ $? -ne 0 ]]; then
-    exit 1
-fi
-echo "Auth is up"
+# ./wait-for-it.sh 127.0.0.1:9099 -t 60
+# if [[ $? -ne 0 ]]; then
+#     exit 1
+# fi
+# echo "Auth is up"
 
 echo "Waiting for UI..."
 ./wait-for-it.sh localhost:3000 -t 60
@@ -23,3 +23,5 @@ echo "UI is up"
 
 echo "Starting tests"
 yarn test;
+
+yarn kill-ui;
